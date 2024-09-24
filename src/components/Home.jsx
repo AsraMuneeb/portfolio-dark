ed74df0bf04a0f91375bc92f461c8900f7595bca
= <=>
2024-09-25 02:53:02 +0500

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,16 +11,14 @@ const Home = () => {
   const contact = useRef(null)
   return (
       <Container fluid className="hero-section text-left">
-      <Row>
-        <Col  className='content'>
+        <div  className='content'>
           <h1 className="hero-title">I'm <span className='name'>Asra Muneeb</span></h1>
           <p className="hero-subtitle">A Front-End Developer, bringing Your Vision to Life Through Modern Web Development.</p>
           <a href="#projects" className="btn btn-custom">See My Work </a>
-        </Col>
-        <Col  className='image'>
+        </div>
+        <div  className='image'>
         <img src={img2} className='image2' />
-        </Col>
-      </Row>
+        </div>
     </Container>
   )
 }
